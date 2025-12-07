3b5fe034bcc9402af0ed7d6f81ce89c3e899f60f
maarten <maarten1096@gmail.com>
2025-12-07 11:26:19 +0100

    update

--- app/ai/genkit.ts
+++ app/ai/genkit.ts
@@ -4,10 +4,8 @@ import {googleAI} from '@genkit-ai/google-genai';
 const geminiApiKey = process.env.GEMINI_API_KEY || process.env.GEMINI_API_KEY_2;
 
 export const ai = genkit({
-  plugins: [googleAI({ 
-    project: 'study-web-app-426213', 
-    location: 'us-central1',
+  plugins: [googleAI({
     apiKey: geminiApiKey,
   })],
-  model: googleAI.model('gemini-2.5-flash-lite'),
+  model: googleAI.model('gemini-1.5-flash'),
 });
