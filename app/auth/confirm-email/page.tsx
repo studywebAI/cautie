c7d8f46e64fae9efc71b771618d61fdbb8f7054d
maarten <maarten1096@gmail.com>
2025-12-16 20:13:44 +0100

    auto commit

--- app/auth/confirm-email/page.tsx
+++ app/auth/confirm-email/page.tsx
@@ -22,7 +22,7 @@ export default function ConfirmEmailPage({
     const { error } = await supabase.auth.verifyOtp({
       email,
       token,
-      type: 'email',
+      type: 'signup',
     });
 
     if (error) {
